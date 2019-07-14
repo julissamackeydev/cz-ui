0ff73a447fd07762f03e29d5ac3813852fa260ab
julissamackeydev <julissamackeydev@gmail.com>
2019-07-14 16:53:29 -0400

    got the images on news clip thumbnails

--- src/app/latest/latest.component.ts
+++ src/app/latest/latest.component.ts
@@ -14,25 +14,26 @@ export class LatestComponent implements OnInit {
             "description": "CHRISTIAN WILL JOIN THE CAST OF AGRIPPINA AT THE ROYAL OPERA HOUSE IN LONDON",
             "date": "JUL 11 2019",
             "content": "Christian has recently been invited to the Royal Opera House in London to join the cast of Agrippina, which includes Joyce DiDonato, Franco Fagioli, and Ierstyn Davies.   Maxim Emelyanychev conducts.",
-            "image": ""
+            "image": "convent-garden",
             },
             {
             "_id": { "$oid": "5cd897f9d46c231f62354987" },
             "description": "AS A HIRED ASSASSIN...",
             "date": "JUL 11 2019",
             "content": "In review of Opera Theater of St. Louis’ Rigoletto, Broadway World notes, “Christian Zaremba is gracefully menacing as Sparafucile, the hired assassin. On his exit at the end of one scene he holds an almost sub-sonic bass note practically forever.  Read the full review here: https://www.broadwayworld.com/bwwopera/article/BWW-Review-A-Glorious-RIGOLETTO-Opens-at-Opera-Theatre-St-Louis-20190604",
-            "image": ""
+            "image": "sparafucille-rigoletto"
             },
             {
             "_id": { "$oid": "5cd897f9d46c231f62354987" },
             "description": "SONGS & DANCES OF DEATH",
             "date": "JUL 11 2019",
             "content": "Christian will sing Modest Mussorgski’s famous song cycle for bass, Songs and Dances of Death, with the Greenwich Symphony in late November 2019.  Please check later for scheduling updates.",
-            "image": ""
+            "image": "mussorgsky"
             }
           ];
 
   ngOnInit() {
+
   }
 
 }
