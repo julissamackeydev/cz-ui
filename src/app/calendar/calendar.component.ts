8148c3fa14e3c84185e5ed526becbe9f0ae95c90
julissamackeydev <julissamackeydev@gmail.com>
2019-12-07 08:35:34 -0500

    introducing previous performances

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -6,39 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  
+
   constructor() { }
   upcoming = [
     {
-        "_id": {
-            "$oid": "5cd8a219d38f5a51aeb17aa3"
-        },
-        "date": "DEC 23 - MAR 7 '20",
-        "location": "METROPOLITAN OPERA",
-        "city": "NEW YORK CITY",
-        "event": "AGRIPPINA",
-        "tickets": "https://www.metopera.org/"
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "DEC 23 - MAR 7 '20",
+      "location": "METROPOLITAN OPERA",
+      "city": "NEW YORK CITY",
+      "event": "AGRIPPINA",
+      "tickets": "https://www.metopera.org/"
     },
     {
-        "_id": {
-            "$oid": "5cd8a219d38f5a51aeb17aa3"
-        },
-        "date": "MAR 28 - APR 11 '20",
-        "location": "NASHVILLE OPERA",
-        "city": "NASHVILLE",
-        "event": "RIGOLETTO",
-        "tickets": "http://www.nashvilleopera.org/"
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "MAR 28 - APR 11 '20",
+      "location": "NASHVILLE OPERA",
+      "city": "NASHVILLE",
+      "event": "RIGOLETTO",
+      "tickets": "http://www.nashvilleopera.org/"
     },
     {
-        "_id": {
-            "$oid": "5cd8a219d38f5a51aeb17aa3"
-        },
-        "date": "APR 19 - MAY 30 '20",
-        "location": "L'OPERA DE MONTREAL",
-        "city": "MONTREAL",
-        "event": "DIE ZAUBERFLÖTE",
-        "tickets": "https://www.operademontreal.com/en"
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "APR 19 - MAY 30 '20",
+      "location": "L'OPERA DE MONTREAL",
+      "city": "MONTREAL",
+      "event": "DIE ZAUBERFLÖTE",
+      "tickets": "https://www.operademontreal.com/en"
     }
+  ]
+  previous = [{
+    "_id": {
+      "$oid": "5cd8a219d38f5a51aeb17aa3"
+    },
+    "date": "OCT 19 - NOV 17 2019",
+    "location": "LYRIC OPERA",
+    "city": "KANSAS CITY",
+    "event": "LA BOHEME",
+    "tickets": "https://kcopera.org/"
+  },{
+    "_id": {
+      "$oid": "5cd8a219d38f5a51aeb17aa3"
+    },
+    "date": "AUG 19 - OCT 12 2019",
+    "location": "ROYAL OPERA HOUSE",
+    "city": "LONDON",
+    "event": "AGRIPPINA",
+    "tickets": "https://www.roh.org.uk/"
+  }
   ]
   ngOnInit() {
 
