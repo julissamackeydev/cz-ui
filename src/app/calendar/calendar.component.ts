53a4c0a4ad78f08ee0ff28943504645edfa78633
julissamackey <julissamackey@gmail.com>
2020-09-13 07:36:32 -0400

    feat: update calendar and add covid quote

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -13,17 +13,17 @@ export class CalendarComponent implements OnInit {
       "_id": {
         "$oid": "5cd8a219d38f5a51aeb17aa3"
       },
-      "date": "DEC 23 - MAR 7 '20",
+      "date": "MAR 2 - MAR 28 '21",
       "location": "METROPOLITAN OPERA",
       "city": "NEW YORK CITY",
-      "event": "AGRIPPINA",
-      "tickets": "https://www.metopera.org/"
+      "event": "GIULIO CESARE",
+      "tickets": "https://www.metopera.org/season/2020-21-season/giulio-cesare/"
     },
     {
       "_id": {
         "$oid": "5cd8a219d38f5a51aeb17aa3"
       },
-      "date": "MAR 28 - APR 11 '20",
+      "date": "APR 8 + APR 10 '21",
       "location": "NASHVILLE OPERA",
       "city": "NASHVILLE",
       "event": "RIGOLETTO",
@@ -33,7 +33,29 @@ export class CalendarComponent implements OnInit {
       "_id": {
         "$oid": "5cd8a219d38f5a51aeb17aa3"
       },
-      "date": "APR 19 - MAY 30 '20",
+      "date": "[TBD] NOV 2021",
+      "location": "AUSTIN OPERA",
+      "city": "AUSTIN",
+      "event": "LE NOZZE DI FIGARO",
+      "tickets": "https://my.austinopera.org/events?view=list"
+    },
+    {
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "[TBD] DEC 2021",
+      "location": "THÉÂTRE DU CAPITOLE",
+      "city": "TOULOUSE",
+      "event": "DIE ZAUBERFLÖTE",
+      "tickets": "https://www.theatreducapitole.fr/"
+    }
+  ]
+  previous = [
+    {
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "[CANCELLED] APR 19 - MAY 30 '20",
       "location": "L'OPERA DE MONTREAL",
       "city": "MONTREAL",
       "event": "DIE ZAUBERFLÖTE",
@@ -43,32 +65,30 @@ export class CalendarComponent implements OnInit {
       "_id": {
         "$oid": "5cd8a219d38f5a51aeb17aa3"
       },
-      "date": "NOV 7 - 15 '20",
-      "location": "AUSTIN OPERA",
-      "city": "AUSTIN",
-      "event": "LE NOZZE DI FIGARO",
-      "tickets": "https://my.austinopera.org/events?view=list"
+      "date": "DEC 23 - MAR 7 '20",
+      "location": "METROPOLITAN OPERA",
+      "city": "NEW YORK CITY",
+      "event": "AGRIPPINA",
+      "tickets": "https://www.metopera.org/"
+    }, {
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "OCT 19 - NOV 17 2019",
+      "location": "LYRIC OPERA",
+      "city": "KANSAS CITY",
+      "event": "LA BOHEME",
+      "tickets": "https://kcopera.org/"
+    }, {
+      "_id": {
+        "$oid": "5cd8a219d38f5a51aeb17aa3"
+      },
+      "date": "AUG 19 - OCT 12 2019",
+      "location": "ROYAL OPERA HOUSE",
+      "city": "LONDON",
+      "event": "AGRIPPINA",
+      "tickets": "https://www.roh.org.uk/"
     }
-  ]
-  previous = [{
-    "_id": {
-      "$oid": "5cd8a219d38f5a51aeb17aa3"
-    },
-    "date": "OCT 19 - NOV 17 2019",
-    "location": "LYRIC OPERA",
-    "city": "KANSAS CITY",
-    "event": "LA BOHEME",
-    "tickets": "https://kcopera.org/"
-  },{
-    "_id": {
-      "$oid": "5cd8a219d38f5a51aeb17aa3"
-    },
-    "date": "AUG 19 - OCT 12 2019",
-    "location": "ROYAL OPERA HOUSE",
-    "city": "LONDON",
-    "event": "AGRIPPINA",
-    "tickets": "https://www.roh.org.uk/"
-  }
   ]
   ngOnInit() {
 
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPageComponent implements OnInit {
 
   constructor() { }
-  location = `NASHVILLE OPERA`;
-  opera = `RIGOLETTO`;
-  startDate = `MAR 28`;
-  endDate = `APR 11 '20`;
-  tickets = `https://www.nashvilleopera.org`;
+  location = `METROPOLITAN OPERA`;
+  opera = `GIULIO CESARE`;
+  startDate = `MAR 2`;
+  endDate = `MAR 21 2021`;
+  tickets = `https://www.metopera.org/season/2020-21-season/giulio-cesare/`;
   upcoming = [
     {
         "_id": {
@@ -57,11 +57,11 @@ export class LandingPageComponent implements OnInit {
   ];
   bioContent =[`Whether “stealing the evening. . . as the dancing master” (The Washington Post) or “chilling in espousing the theories of Negro management” (Opera News), French-American bass, Christian Zaremba, is intent on leaving a strong impression each time he performs.
   Last season, Mr. Zaremba was seen as Angelotti in Tosca at the Metropolitan Opera, Sparafucile in Rigoletto with Michigan Opera Theater, Zuniga in Carmen with Austin Opera, and the bass soloist in Verdi’s Requiem with The Greenwich Symphony.  Recent highlights include Il Re in Aïda with the National Symphony, Collatinus in The Rape of Lucretia, Passagallo in L’Opera Seria, and Dr. Bartolo in Le nozze di Figaro with Wolf Trap Opera, Colline in La bohème with Portland Opera and Opera Omaha, Sarastro in Die Zauberflöte and Ashby in La fanciulla del West with Minnesota Opera, and the Bass Soloist in The Little Match Girl Passion with the Glimmerglass Festival and Portland Opera.`,
-   
+
   `In the 2018-2019 season, Mr. Zaremba returns to The Metropolitan for productions of Gianni Schicchi and Tosca before greeting the New Year with a return to Austin Opera as the French General in Silent Night and to Minnesota Opera as Mountain Landis in the world premier of The Fix by Joel Puckett.  He then makes debuts with The Lyric Opera of Kansas City as Nouribad in Les pêcheurs des perles and reprises his Sparafucile in Rigoletto with Opera Theatre of St. Louis.`,
-  
+
   `Future seasons will see him join the cast of Agrippina at the Royal Opera House in London, as Colline in La bohème with Lyric Opera of Kansas City, Lesbo in Agrippina with the Metropolitan Opera, Sparafucile in Rigoletto with Nashville Opera, Sarastro in Die Zauberflöte with Opéra de Montreal, and in a much-anticipated titular role debut with Austin Opera.`];
-  
+
   newsClips = [
     {
       "_id": { "$oid": "st-louis" },
